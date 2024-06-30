92aa42ca46eb250cc65fe612b8f1b7ecb2f7cbc2
Gyakobo <andygyakobo@gmail.com>
2024-06-30 11:32:39 -0700

    Finished writing the insertion sort

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ def selection_sort(arr):
         
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
 
 
 
-
-
+arr = [5, 3, 2, 1, 4, 6]
+insertion_sort(arr)
+print(arr)
